a055e1842ad66a7ae301319d5e70a62dec23a56f
nautilus@gideboctopus <nautilus@trovaprezzi.it>
2024-11-11 17:13:23 +0100

    Prove

--- Gemfile
+++ Gemfile
@@ -13,12 +13,7 @@ gem 'json-api-vanilla' # Per il parsing della risposta
 # Server
 gem 'active_model_serializers', '0.10.14' # JSON:API 1.0
 
-# Per la URI
-# application/x-www-form-urlencoded
-# application/x-www-form-urlencoded serializer
-# Usare URI.decode_www_form  (bizarre historical artifact that + characters must be treated as spaces)
 
 group(:test) do
   gem('rspec-rails')
-  #gem 'rspec-expectations'
 end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::PostsController < Api::ApiController
     post2 = Post.new(2, 'Title 2', 'Text 2', author)
     posts = [post1, post2]
 
-    options = Adapters::JsonApi.new(params).options
-    base = { json: posts, adapter: :json_api, status: 200}.merge(options)
+    params_options = ::Adapters::JsonApi.new(params).options
+    options = { json: posts, adapter: :json_api, status: 200}.merge(params_options)
 
-    render(base)
+    render(options)
   end
 end
